aa92e24deda7e1ea50156792aa445ddfb7c54cd4
mariahiguera2210 <angelica.higueram92@gmail.com>
2023-11-24 18:21:50 -0500

    se crean las clases AcademicManager, Course y Student con sus respectivos atributos

---
+++ src/main/java/org/example/Course.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Course {
+
+    private int idCourse;
+    private String nameCourse;
+    private String description;
+    private int credits;
+    private int vertion;
+
+
+
+    List<String> coursesList = new ArrayList<>();
+}
---
+++ src/main/java/org/example/Student.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Student {
+
+    private int idStudent;
+    private String name;
+    private String lastName;
+    private String birthdayDate;
+    private String state;
+
+    List<String> studentsList = new ArrayList<>();
+}
