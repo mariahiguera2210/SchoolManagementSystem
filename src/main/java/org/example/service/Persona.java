f202bd0cb0ee88a362516522e0ab028fd5eef90c
mariahiguera2210 <angelica.higueram92@gmail.com>
2023-11-25 23:09:16 -0500

    se agrega clase Persona

--- src/main/java/org/example/service/Persona.java
+++ src/main/java/org/example/service/Persona.java
@@ -1,2 +1,10 @@
-package org.example.service;public class Persona {
+package org.example.service;
+
+import org.example.Student;
+
+public class Persona extends Student {
+
+    public Persona(int idStudent, String name, String lastName, String birthdayDate, State state) {
+        super(idStudent, name, lastName, birthdayDate, state);
+    }
 }
